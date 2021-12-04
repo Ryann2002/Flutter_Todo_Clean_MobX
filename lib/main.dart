370e6af59f57625a04aab5596a649c6f61e5c291
Ryan Ferreira <ryanferreira135@gmail.com>
2021-12-04 15:00:58 -0300

    removed app_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'app/app_widget.dart';
+import 'app/home/presenters/pages/home_page.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -10,4 +10,18 @@ void main() async {
   runApp(const AppWidget()
       // ModularApp(module: AppModule(), child: const AppWidget()),
       );
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Slidy',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
 }
